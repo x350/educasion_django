1207a7f1fafc2a3423f2937e5f8f244f0a907b64
Dmitrii Shuliak <x350sh@gmail.com>
2025-06-21 23:30:07 +0300

    added django project with models

---
+++ mysite/education/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from .models import Course, Student, Teacher, Lesson, Category, LessonFiles, StudentsGroup
+
+
+# Register your models here.
+class LessonInline(admin.TabularInline):
+    model = LessonFiles
+
+class StudentInline(admin.StackedInline):
+    model = StudentsGroup
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = "pk", "title", "description", "category", "created", "update", "is_active"
+    list_display_links = "pk", "title", "category", "is_active"
+    ordering = "pk", "-title"
+    search_fields = "title", "description",
+
+    inlines = [
+        StudentInline
+    ]
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = "pk", "title", "teacher", "photo",
+    inlines = [
+        LessonInline,
+    ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "title",
+    list_display_links = "title",
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    pass
---
+++ mysite/education/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+# from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+# Create your models here.
+# UserModel = get_user_model()
+
+
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название дисциплины')
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название курса')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание курса')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='courses')
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название урока')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание урока')
+    teacher = models.OneToOneField(Teacher, on_delete=models.DO_NOTHING)
+    photo = models.ImageField(null=True, upload_to='photos/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class StudentsGroup(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, related_name='students')
+
+
+def lesson_files_path(instance: "LessonFiles", filename: str) -> str:
+    return "courses/course_{pk}/files/{filename}".format(
+        pk=instance.lesson.pk,
+        filename=filename
+    )
+
+
+class LessonFiles(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='files')
+    file = models.FileField(upload_to=lesson_files_path)
+    description = models.CharField(max_length=200, null=False, blank=True)
+
+
+
+
+
+
